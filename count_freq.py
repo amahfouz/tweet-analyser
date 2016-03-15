e8d68214a1ba959889a011c2b5700e0cc70fc875
Ayman Mahfouz <amahfouz@gmail.com>
2016-03-14 18:15:23 -0700

    Read file name from command line. Removed duplicate word from words file.

--- count_freq.py
+++ count_freq.py
@@ -6,6 +6,7 @@
 import codecs
 import sys
 import re
+import time
 
 def count_occurrences(f, w):
 	count = 0
@@ -23,14 +24,22 @@ def count_occurrences(f, w):
 	Iterate over all words and find each in the file
 '''
 
+start = time.time()
+
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
+if len(sys.argv) < 2:
+	print "Please provide filename as a command argument"
+	sys.exit(100)
+
 word_file = codecs.open("words.txt", 'r', 'utf-8')
 for word in word_file:
-	f = open("eg-tweets.txt")
+	f = open(sys.argv[1])
 	print word.rstrip(), "\t\t", count_occurrences(f, word.rstrip())
 	f.close()
 
+end = time.time()
 
+print end - start, " seconds"
 
